5d6ba2d174208588eb70b73673d5b185bb815f5c
jazibsiddique02 <rajajazibsiddique@gmail.com>
2024-12-04 11:10:24 +0500

    Add project files.

---
+++ FYP(Reward Based Crowdfunding)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FYP_Reward_Based_Crowdfunding_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
